53ac1e2d46ecf39d98a48963e10fee13663d0436
katthigokul <gokulkatthi@gmail.com>
2019-08-18 22:31:24 +0530

    Created RabbitMQ consumer and added gitignore

---
+++ src/main/java/com/stackroute/config/RabbitMqConfig.java
@@ -0,0 +1,49 @@
+package com.stackroute.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class RabbitMqConfig {
+
+    /**
+     * @Value injects values into fields
+     */
+    @Value("${jsa.rabbitmq.queue}")
+    private String queueName;
+
+    @Value("${jsa.rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${jsa.rabbitmq.routingkey}")
+    private String routingKey;
+
+    /**
+     * @Bean Creates required objects
+     */
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(exchange);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+}
