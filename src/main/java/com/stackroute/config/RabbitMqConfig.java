013283cf1f7f4259de05e7fcff4f2c5afcbd47d6
katthigokul <gokulkatthi@gmail.com>
2019-08-21 10:09:02 +0530

    changes in consumer and producer

--- src/main/java/com/stackroute/config/RabbitMqConfig.java
+++ src/main/java/com/stackroute/config/RabbitMqConfig.java
@@ -1,16 +1,21 @@
 package com.stackroute.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Indicates this as a configuration class
+ */
 
 @Configuration
 public class RabbitMqConfig {
-
     /**
      * @Value injects values into fields
      */
@@ -46,4 +51,18 @@ public class RabbitMqConfig {
         return new Jackson2JsonMessageConverter();
     }
 
+    @Bean
+    ConnectionFactory connectionFactory() {
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory("localhost");
+        cachingConnectionFactory.setUsername("guest");
+        cachingConnectionFactory.setPassword("guest");
+        return cachingConnectionFactory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
 }
